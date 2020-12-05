2c387d6614a132ecde2e119b8587432f89efbd06
CinarIoan <cinar.ioan@gmail.com>
2020-12-05 10:10:16 +0200

    first commit

---
+++ Product.cs
@@ -0,0 +1,14 @@
+using System
+
+namespace VengindMachine
+
+{
+	class Product
+	{
+		public string Name { get; set;}
+		public double Price { get; set;}
+		public int Quantity { get; set;}
+		
+	
+	}
+}
